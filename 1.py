48000335747a8e2f1054238494cb4ba043e61e91
raju <nerugattiraju123@gmail.com>
2019-11-25 00:46:54 +0530

    second commit

--- 1.py
+++ 1.py
@@ -1,2 +1,9 @@
-print('hii')
-print('raju')
+def outer(func):
+    def inner():
+        x=func()
+        return x.upper()
+    return inner
+@outer
+def wish():
+    return 'hii good morning'
+print(wish())
